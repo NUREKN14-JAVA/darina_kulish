cf655b4b080682a240865ec65422f19eaf3a8816
Darina Kulish <leopersica@rambler.ru>
2016-12-02 01:10:05 +0300

    Refactor DaoFacoty, new DaoFactoryImpl

---
+++ usermanagement/src/test/java/kn/darina_kulish/usermanagement/db/DaoFactoryImpl.java
@@ -0,0 +1,11 @@
+package kn.darina_kulish.usermanagement.db;
+
+public class DaoFactoryImpl extends DaoFactory {
+
+	@Override
+	public UserDao getUserDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
