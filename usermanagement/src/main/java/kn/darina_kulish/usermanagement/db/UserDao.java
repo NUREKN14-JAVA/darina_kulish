02b16d2362c63dd6a7f9143e685a260167997732
Darina Kulish <leopersica@rambler.ru>
2016-12-01 23:01:38 +0300

    Some tests, e.g. for create()

---
+++ usermanagement/src/main/java/kn/darina_kulish/usermanagement/db/UserDao.java
@@ -0,0 +1,13 @@
+package kn.darina_kulish.usermanagement.db;
+
+import java.util.Collection;
+
+import kn.darina_kulish.usermanagement.User;
+
+public interface UserDao {
+User create(User user) throws DatabaseException;
+void update(User user) throws DatabaseException;
+void delete(User user) throws DatabaseException;
+User find(Long id) throws DatabaseException;
+Collection findAll() throws DatabaseException;
+}
