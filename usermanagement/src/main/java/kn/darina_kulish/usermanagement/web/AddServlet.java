d28ad41c3eb13a6035e33120a680b5eb42a88590
Darina <leopersica@rambler.ru>
2016-12-11 21:31:31 +0300

    Lab4 is done!

---
+++ usermanagement/src/main/java/kn/darina_kulish/usermanagement/web/AddServlet.java
@@ -0,0 +1,24 @@
+package kn.darina_kulish.usermanagement.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kn.darina_kulish.usermanagement.User;
+import kn.darina_kulish.usermanagement.db.DaoFactory;
+import kn.darina_kulish.usermanagement.db.DatabaseException;
+
+public class AddServlet extends EditServlet {
+
+    protected void processUser(User user) throws DatabaseException {
+        DaoFactory.getInstance().getUserDao().create(user);
+    }
+
+    protected void showPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/add.jsp").forward(req, resp);
+    }
+}
