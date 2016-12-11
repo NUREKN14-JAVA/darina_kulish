b0cbe0e718496271a3acc3a1ae16009cab0b8f9c
Darina <leopersica@rambler.ru>
2016-12-11 21:47:48 +0300

    Lab5 is done!

---
+++ usermanagement/src/main/java/kn/darina_kulish/usermanagement/agent/SearchException.java
@@ -0,0 +1,13 @@
+package kn.darina_kulish.usermanagement.agent;
+
+import kn.darina_kulish.usermanagement.db.DatabaseException;
+
+public class SearchException extends Exception {
+
+	public SearchException(DatabaseException e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
